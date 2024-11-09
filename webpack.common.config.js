574e77eb2118402c9f71702510a4518697c65571
MrDigger <mrdigger@mail.ru>
2024-11-09 12:27:17 +0300

    Update dependencies.

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -11,77 +11,86 @@ const CleanWebpackPlugin = require('clean-webpack-plugin'); // Clears (removes) 
  * @return {Object} webpack.config
  */
 module.exports = (props) => {
-    const {
-        mode = process.env.NODE_ENV || 'development',
-        contentBase = 'dist',
-        cleanContent = true,
-    } = props || {};
+  const {
+    mode = process.env.NODE_ENV || 'development',
+    contentBase = 'dist',
+    cleanContent = true,
+  } = props || {};
 
-    const plugins = cleanContent ? [
-        new CleanWebpackPlugin([ contentBase ]), // <--- @warn: Clears the folder]
-    ] : [];
+  const plugins = cleanContent
+    ? [
+        new CleanWebpackPlugin([contentBase]), // <--- @warn: Clears the folder]
+      ]
+    : [];
 
-    return {
-        mode, // "production" | "development" | "none"
-        entry: {
-          // mylib: './src/index.js',
+  return {
+    mode, // "production" | "development" | "none"
+    entry: {
+      // mylib: './src/index.js',
+    },
+    devtool: mode === 'development' ? 'inline-source-map' : false,
+    devServer: {
+      static: contentBase,
+      port: 5001,
+    },
+    plugins,
+    output: {
+      filename: '[name].bundle.js', // <--- myapp1.bundle.js
+      path: path.resolve(__dirname, contentBase),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
         },
-        devtool: mode === 'development' ? 'inline-source-map' : false,
-        devServer: {
-            contentBase,
-            port: 5000
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader', // compiles Sass to CSS, using Node Sass by default
+            {
+              loader: 'postcss-loader',
+              options: { postcssOptions: { parser: 'postcss-scss' } },
+            },
+          ],
         },
-        plugins,
-        output: {
-            filename: '[name].bundle.js',   // <--- myapp1.bundle.js
-            path: path.resolve(__dirname, contentBase)
+        {
+          // Insert images by link.
+          test: /\.(png|svg|jpg|gif)$/,
+          type: 'asset/resource',
+          // use: ['file-loader'] // webpack v.4
         },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use: [
-                        'style-loader',
-                        'css-loader'
-                    ]
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        'style-loader', // creates style nodes from JS strings
-                        'css-loader',   // translates CSS into CommonJS
-                        'sass-loader'   // compiles Sass to CSS, using Node Sass by default
-                    ]
-                },
-                {
-                    test: /\.less$/,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        'less-loader'
-                    ]
-                },
-                {
-                    test: /\.(png|svg|jpg|gif)$/,
-                    use: [
-                        'file-loader'
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                    use: [
-                        'file-loader'
-                    ]
-                },
-                {
-                    test: /\.ts?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/
-                }
-            ]
+        {
+          // Insert inline images (direct in HTML).
+          test: /\.inline\.(png|svg|jpg|gif)$/,
+          type: 'asset/inline',
         },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js']
-        }
-    }
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          // use: ['file-loader'] // webpack v.4
+        },
+        {
+          test: /\.html$/,
+          loader: 'underscore-template-loader',
+          // options: { engine: 'lodash' },
+          exclude: /index\.html$/,
+        },
+        {
+          test: /\.ts?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader', // Normal loader, NOT suitable for VueJS!
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const CopyWebpackPlugin   = require('copy-webpack-plugin');  // Copy files to output folder
-const webpackMerge        = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Copy files to output folder
+const webpackMerge = require('webpack-merge');
 const createWebpackConfig = require('./webpack.common.config');
 
 /**
  * Configuration to build the library.
  */
 module.exports = webpackMerge.merge(createWebpackConfig({ contentBase: 'dist' }), {
-    entry: {
-        lib: './src/lib.ts',
-    },
-    output: {
-        library: 'atracktor',
-        libraryTarget: 'umd',
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: 'package.json', to: 'package.json' },
-            { from: 'README.npm.md', to: 'README.md' },
-            { from: 'LICENSE', to: '[name]' },
-        ]),
-    ],
+  entry: {
+    lib: './src/lib.ts',
+  },
+  output: {
+    library: 'atracktor',
+    libraryTarget: 'umd',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'package.json', to: 'package.json' },
+      { from: 'README.npm.md', to: 'README.md' },
+      { from: 'LICENSE', to: '[name]' },
+    ]),
+  ],
 });
